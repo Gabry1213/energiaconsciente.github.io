59b8025ed9f3772690667aa4f939529e09cc95c6
João Pedro <jpfs@icomp.ufam.edu.br>
2020-08-02 22:37:51 -0400

    footer

--- jogos/cacapalavra/js/wordpaths.js
+++ jogos/cacapalavra/js/wordpaths.js
@@ -10,18 +10,16 @@
  * 'backwards' at the end of the string refers to it going the opposite direction (so
  *  bottom -> top or lower left -> upper right)
  */
-var paths = { 
+var paths = {
+  vert: "vertical",
+  horizon: "horizontal",
+  // priDiag: "primaryDiagonal",
+  // secDiag: "secondaryDiagonal",
 
-	vert: "vertical",
-	horizon: "horizontal",
-	priDiag: "primaryDiagonal",
-	secDiag: "secondaryDiagonal",
-
-	vertBack: "verticalBackwards",
-	horizonBack: "horizonBackwards",
-	priDiagBack: "primaryDiagonalBackwards",
-	secDiagBack: "secondaryDiagonalBackwards",
-
+  vertBack: "verticalBackwards",
+  horizonBack: "horizonBackwards",
+  //   priDiagBack: "primaryDiagonalBackwards",
+  //   secDiagBack: "secondaryDiagonalBackwards",
 };
 
 /** this object sets up the matrix bounds for each orientation (just to ensure when inserting
@@ -31,37 +29,33 @@ var paths = {
  * @param {Number} y column of current matrix index
  * @param {Number} s size (width or height, either one as they should be equal) of the matrix of letters
  */
-var bounds = { 
+var bounds = {
+  [paths.vert]: (x, y, s) => x < s,
+  [paths.horizon]: (x, y, s) => y < s,
+  [paths.priDiag]: (x, y, s) => x < s && y < s,
+  [paths.secDiag]: (x, y, s) => x < s && y >= 0,
 
-	[paths.vert]: (x, y, s) => (x < s),
-	[paths.horizon]: (x, y, s) => (y < s),
-	[paths.priDiag]: (x, y, s) => (x < s) && (y < s),
-	[paths.secDiag]: (x, y, s) =>  (x < s) && (y >= 0),
-
-	[paths.vertBack]: (x, y, s) => (x >= 0),
-	[paths.horizonBack]: (x, y, s) => (y >= 0),
-	[paths.priDiagBack]: (x, y, s) => (x >= 0) && (y >= 0),
-	[paths.secDiagBack]: (x, y, s) => (x >= 0) && (y < s)
-
+  [paths.vertBack]: (x, y, s) => x >= 0,
+  [paths.horizonBack]: (x, y, s) => y >= 0,
+  [paths.priDiagBack]: (x, y, s) => x >= 0 && y >= 0,
+  [paths.secDiagBack]: (x, y, s) => x >= 0 && y < s,
 };
 
-/** this object takes the given matrix row/colum and increments it in the 
+/** this object takes the given matrix row/colum and increments it in the
  * direction of the path given
  *
  * @param {Number} x matrix row to increment
  * @param {Number} y matrix column to increment
  * @return incremented x and y coordinates (by a factor of 1)
  */
-var incr = { 
+var incr = {
+  [paths.vert]: (x, y) => ({ x: x + 1, y: y }),
+  [paths.horizon]: (x, y) => ({ x: x, y: y + 1 }),
+  [paths.priDiag]: (x, y) => ({ x: x + 1, y: y + 1 }),
+  [paths.secDiag]: (x, y) => ({ x: x + 1, y: y - 1 }),
 
-	[paths.vert]: (x, y) => ({x: x+1, y: y}),
-	[paths.horizon]: (x, y) => ({x: x, y: y+1}),
-	[paths.priDiag]: (x, y) => ({x: x+1, y: y+1}),
-	[paths.secDiag]: (x, y) => ({x: x+1, y: y-1}),
-
-	[paths.vertBack]: (x, y) => ({x: x-1, y: y}),
-	[paths.horizonBack]: (x, y) => ({x: x, y: y-1}),
-	[paths.priDiagBack]: (x, y) => ({x: x-1, y: y-1}),
-	[paths.secDiagBack]: (x, y) => ({x: x-1, y: y+1})
-
+  [paths.vertBack]: (x, y) => ({ x: x - 1, y: y }),
+  [paths.horizonBack]: (x, y) => ({ x: x, y: y - 1 }),
+  [paths.priDiagBack]: (x, y) => ({ x: x - 1, y: y - 1 }),
+  [paths.secDiagBack]: (x, y) => ({ x: x - 1, y: y + 1 }),
 };
